b9ecb33ec3718b18904d209ef835903081a09dc1
joseyawovi <joseyawovi@gmail.com>
2025-01-16 10:16:50 +0530

    adding gunicorn to the requirements

--- french/wsgi.py
+++ french/wsgi.py
@@ -4,7 +4,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 environment = os.getenv('DJANGO_ENV', 'development')  # Default to 'development'
-settings_module = f'your_project_name.settings.{environment}'
+settings_module = f'french.settings.{environment}'
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
